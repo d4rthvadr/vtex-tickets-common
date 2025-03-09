a9b67a6f8d3e7168664b3fa008401c0e3c2b4d1a
darkvadr <glowburn.acc@gmail.com>
2025-03-09 09:45:19 +0000

    simplify id generator

--- src/utils/id-generator.ts
+++ src/utils/id-generator.ts
@@ -1,6 +1,6 @@
 import { randomBytes } from "crypto";
 
-export const genID = (prefix: string = "") => {
-  const id = `${randomBytes(6).toString("hex")}`;
+export const genID = (prefix: string = ""): string => {
+  const id = randomBytes(6).toString("hex");
   return prefix.length ? `${prefix}_${id}` : id;
 };
